30846ccac881bb02470727c392c301dce89f6e09
elegant <illds@mail.ru>
2024-01-30 13:32:48 +0400

    Job pages upgraded

--- sims/jobs/routes.py
+++ sims/jobs/routes.py
@@ -33,97 +33,96 @@ def job(job_id):
     return render_template('jobs/job.html', job=job)
 
 
-@jobs.route("/family/<int:family_id>/update", methods=['GET', 'POST'])
+@jobs.route("/job/<int:job_id>/update", methods=['GET', 'POST'])
 @login_required
-def update_family(family_id):
-    family = Family.query.get_or_404(family_id)
+def update_job(job_id):
+    job = Jobs.query.get_or_404(job_id)
 
-    form = FamilyForm()
+    form = JobForm()
     if form.validate_on_submit():
-        family.name = form.name.data
+        job.name = form.name.data
+        job.salary = form.salary.data
         db.session.commit()
-        flash('Your family has been updated!', 'success')
-        return redirect(url_for('jobs.family', family_id=family.id))
+        flash('Your job has been updated!', 'success')
+        return redirect(url_for('jobs.job', job_id=job.id))
     elif request.method == 'GET':
-        form.name.data = family.name
+        form.name.data = job.name
+        form.salary.data = job.salary
 
-    return render_template('jobs/create_family.html', form=form, legend='Update Family', title='Update Family')
+    return render_template('jobs/create_job.html', form=form, legend='Update Job', title='Update Job')
 
 
-@jobs.route("/family/<int:family_id>/delete", methods=['POST'])
+@jobs.route("/job/<int:job_id>/delete", methods=['POST'])
 @login_required
-def delete_family(family_id):
-    family = Family.query.get_or_404(family_id)
+def delete_job(job_id):
+    job = Jobs.query.get_or_404(job_id)
 
-    Pet.query.filter_by(family_id=family.id).update({Pet.family_id: None})
-    Human.query.filter_by(family_id=family.id).update({Human.family_id: None})
-
-    db.session.delete(family)
+    db.session.delete(job)
     db.session.commit()
-    flash('Family has been deleted!', 'success')
+    flash('Job has been deleted!', 'success')
     return redirect(url_for('main.home'))
 
 
 @jobs.route("/jobs")
 @login_required
 def all_jobs():
-    jobs = Family.query.all()
+    jobs = Jobs.query.all()
     return render_template('jobs/jobs.html', title='List of jobs',
                            jobs=jobs)
-
-
-@jobs.route("/jobs/human/<int:human_id>", methods=['GET', 'POST'])
-@login_required
-def jobs_human(human_id):
-    jobs = Family.query.all()
-    return render_template('jobs/jobs_human.html', title='List of jobs',
-                           jobs=jobs, human_id=human_id)
-
-
-@jobs.route("/jobs/pet/<int:pet_id>", methods=['GET', 'POST'])
-@login_required
-def jobs_pet(pet_id):
-    jobs = Family.query.all()
-    return render_template('jobs/jobs_pet.html', title='List of jobs',
-                           jobs=jobs, pet_id=pet_id)
-
-
-@jobs.route("/jobs/house/<int:house_id>", methods=['GET', 'POST'])
-@login_required
-def jobs_house(house_id):
-    jobs = Family.query.all()
-    return render_template('jobs/jobs_house.html', title='List of jobs',
-                           jobs=jobs, house_id=house_id)
-
-
-@jobs.route("/family/human_join/<int:family_id>/<int:human_id>", methods=['GET', 'POST'])
-@login_required
-def human_join_family(family_id, human_id):
-    human = Human.query.get_or_404(human_id)
-    family = Family.query.get_or_404(family_id)
-
-    if human.surname != family.name:
-        flash(f'{human.name} doesn\'t have {family.name} surname.', 'danger')
-        return redirect(url_for('humans.human', human_id=human.id))
-
-
-    human.family_id = family_id
-    db.session.commit()
-
-    flash(f'{human.name} has joined {family.name} family!', 'success')
-    return redirect(url_for('humans.human', human_id=human.id))
-
-
-@jobs.route("/family/pet_join/<int:family_id>/<int:pet_id>", methods=['GET', 'POST'])
-@login_required
-def pet_join_family(family_id, pet_id):
-    pet = Pet.query.get_or_404(pet_id)
-    family = Family.query.get_or_404(family_id)
-
-    pet.family_id = family_id
-    db.session.commit()
-
-    flash(f'{pet.name} has joined {family.name} family!', 'success')
-    return redirect(url_for('pets.pet', pet_id=pet.id))
-
-
+#
+#
+# @jobs.route("/jobs/human/<int:human_id>", methods=['GET', 'POST'])
+# @login_required
+# def jobs_human(human_id):
+#     jobs = Family.query.all()
+#     return render_template('jobs/jobs_human.html', title='List of jobs',
+#                            jobs=jobs, human_id=human_id)
+#
+#
+# @jobs.route("/jobs/pet/<int:pet_id>", methods=['GET', 'POST'])
+# @login_required
+# def jobs_pet(pet_id):
+#     jobs = Family.query.all()
+#     return render_template('jobs/jobs_pet.html', title='List of jobs',
+#                            jobs=jobs, pet_id=pet_id)
+#
+#
+# @jobs.route("/jobs/house/<int:house_id>", methods=['GET', 'POST'])
+# @login_required
+# def jobs_house(house_id):
+#     jobs = Family.query.all()
+#     return render_template('jobs/jobs_house.html', title='List of jobs',
+#                            jobs=jobs, house_id=house_id)
+#
+#
+# @jobs.route("/family/human_join/<int:family_id>/<int:human_id>", methods=['GET', 'POST'])
+# @login_required
+# def human_join_family(family_id, human_id):
+#     human = Human.query.get_or_404(human_id)
+#     family = Family.query.get_or_404(family_id)
+#
+#     if human.surname != family.name:
+#         flash(f'{human.name} doesn\'t have {family.name} surname.', 'danger')
+#         return redirect(url_for('humans.human', human_id=human.id))
+#
+#
+#     human.family_id = family_id
+#     db.session.commit()
+#
+#     flash(f'{human.name} has joined {family.name} family!', 'success')
+#     return redirect(url_for('humans.human', human_id=human.id))
+#
+#
+# @jobs.route("/family/pet_join/<int:family_id>/<int:pet_id>", methods=['GET', 'POST'])
+# @login_required
+# def pet_join_family(family_id, pet_id):
+#     pet = Pet.query.get_or_404(pet_id)
+#     family = Family.query.get_or_404(family_id)
+#
+#     pet.family_id = family_id
+#     db.session.commit()
+#
+#     flash(f'{pet.name} has joined {family.name} family!', 'success')
+#     return redirect(url_for('pets.pet', pet_id=pet.id))
+#
+#
